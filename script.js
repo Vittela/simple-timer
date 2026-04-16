3656c3bf92fbf340547f374dc51f5497e2ce472e
Vi <vittela.code@gmail.com>
2026-04-15 22:17:08 -0300

    feat: countdown with seconds, minutes and hours

--- script.js
+++ script.js
@@ -1,13 +1,41 @@
-const timer = document.getElementById("timer-display");
+function Clock(seconds, minutes, hours) {
+  this.seconds = seconds;
+  this.minutes = minutes;
+  this.hours = hours;
+}
 
-let time = 5
+function configurateTime(time) {
+  if (time.seconds >= 60) {
+    time.seconds -= 60;
+    time.minutes += 1;
+  }
+  else if (time.minutes >= 60) {
+    time.minutes -= 60;
+    time.hours += 1;
+  }
+
+  else if (time.minutes >= 0) {
+    if (time.seconds <= 0) {
+      time.seconds += 60;
+      time.minutes -= 1;
+    }
+  }
+
+  else if (time.hours >= 0) {
+    if (time.minutes <= 0) {
+      time.minutes += 60;
+      time.hours -= 1;
+    }
+  }
+}
+
+const clock_1 = new Clock(3, 1, 2)
 const counter = setInterval(countdown, 1000)
 
-console.log(timer.innerHTML)
-
 function countdown() {
-  timer.innerHTML = ("00:00:0" + time)
-  time -= 1
+  configurateTime(clock_1)
+  clock_1.seconds -= 1;
 
-  if (time < 0) { clearInterval(counter) }
-} 
+  if (clock_1.seconds <= 0 && clock_1.minutes <= 0 && clock_1.hours <= 0) { clearInterval(counter) }
+  console.log(clock_1)
+}
