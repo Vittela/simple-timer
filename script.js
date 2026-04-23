e47477eb7f2bb54e1b7028822c478078fa44c3ae
Vi <vittela.code@gmail.com>
2026-04-23 00:25:00 -0300

    feat: definateTime, startCountdown, pauseCountdown, and resetCountdown commands created

--- script.js
+++ script.js
@@ -1,16 +1,68 @@
-let visualTimer = document.getElementById("timer-display");
-let totalSeconds = 4208;
+let display = document.getElementById("timer-display");
+let totalSeconds = 0;
+let initialTime = totalSeconds;
+let timer = null;
+let counting = false;
+display.innerHTML = formatTime(totalSeconds)
 
-setInterval(countdown, 1000)
 
-function countdown() {
-  console.log(derivateTimes(totalSeconds));
-  visualTimer.innerHTML = derivateTimes(totalSeconds)
-  totalSeconds -= 1;
+// Timer functions
+
+function definateTime(hours, minutes, seconds) {
+  counting = false;
+  updateTime();
+
+  totalSeconds = derivateSeconds(hours, minutes, seconds);
+  initialTime = totalSeconds;
+  display.innerHTML = formatTime(totalSeconds);
+
+  return (`New time: ${hours}:${minutes}:${seconds}`);
 }
 
-// Organize and format hours, minutes and seconds.
-function derivateTimes(timeInSeconds) {
+function startCountdown() {
+  counting = true;
+  updateTime();
+
+  return ("Countdown started");
+}
+
+function pauseCountdown() {
+  counting = false;
+  updateTime();
+
+  return ("Countdown paused");
+}
+
+function resetCountdown() {
+  counting = false;
+  updateTime();
+
+  totalSeconds = initialTime;
+  display.innerHTML = formatTime(totalSeconds);
+
+  return ("Countdown restarted");
+}
+
+// Logic functions
+
+function updateTime() {
+  if (counting === true) {
+    timer = setInterval(countdown, 1000);
+  } else {
+    clearInterval(timer);
+    timer = null;
+  }
+}
+
+function countdown() {
+  console.log(formatTime(totalSeconds));
+  display.innerHTML = formatTime(totalSeconds);
+  totalSeconds -= 1;
+
+  if (totalSeconds < 1) { clearInterval(timer) };
+}
+
+function formatTime(timeInSeconds) {
   let hours = Math.trunc((timeInSeconds / 3600));
   let minutes = Math.trunc((timeInSeconds % 3600) / 60);
   let seconds = (timeInSeconds % 3600) % 60;
@@ -19,5 +71,12 @@ function derivateTimes(timeInSeconds) {
   if (`${minutes}`.length === 1) { minutes = `0${minutes}` };
   if (`${seconds}`.length === 1) { seconds = `0${seconds}` };
 
-  return (`${hours}:${minutes}:${seconds}`)
+  return (`${hours}:${minutes}:${seconds}`);
+};
+
+function derivateSeconds(hours, minutes, seconds) {
+  hours = Math.trunc((hours * 3600));
+  minutes = Math.trunc(minutes * 60);
+
+  return (hours + minutes + seconds);
 }
