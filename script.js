c7e654aa7bc340b5fced46a99af48b294b163030
Vi <vittela.code@gmail.com>
2026-04-25 09:42:29 -0300

    feat: definateTime interface created

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 // Interface variables
-const timerDisplay = document.getElementById("timer-display");
+const timeEditor = document.getElementById("time-editor");
+const inputHours = document.getElementById("input-hours");
+const inputMinutes = document.getElementById("input-minutes");
+const inputSeconds = document.getElementById("input-seconds");
 const buttonToggle = document.getElementById("toggle");
 const buttonStop = document.getElementById("stop");
+const timerDisplay = document.getElementById("timer-display")
 
 // Logic variables
 let totalSeconds = 40;
@@ -13,6 +17,67 @@ let counting = false;
 timerDisplay.innerHTML = formatTime(totalSeconds);
 buttonToggle.addEventListener("click", toggleCountdown);
 buttonStop.addEventListener("click", StopCountdown);
+
+[inputHours, inputMinutes, inputSeconds].forEach(input => {
+  input.addEventListener("keydown", (e) => {
+    if (e.key === "Enter") disableEditMode();
+  });
+
+  input.addEventListener("blur", () => {
+    setTimeout(() => {
+      if (!timeEditor.contains(document.activeElement)) {
+        disableEditMode();
+      }
+    }, 100);
+  });
+});
+
+// Interface functions
+
+function enableEditMode() {
+  timerDisplay.style.display = "none";
+  timeEditor.style.display = "block";
+
+  const [h, m, s] = formatTime(initialTime).split(":");
+
+  inputHours.value = Number(h);
+  inputMinutes.value = Number(m);
+  inputSeconds.value = Number(s);
+
+  inputHours.focus();
+}
+
+function disableEditMode() {
+  const h = Number(inputHours.value) || 0;
+  const m = Number(inputMinutes.value) || 0;
+  const s = Number(inputSeconds.value) || 0;
+
+  totalSeconds = derivateSeconds(h, m, s);
+  initialTime = totalSeconds;
+
+  timerDisplay.innerHTML = formatTime(totalSeconds);
+
+  timeEditor.style.display = "none";
+  timerDisplay.style.display = "block";
+}
+
+function updateInterface() {
+  if (counting === true) {
+    buttonToggle.ariaLabel = "Pause"
+    buttonToggle.innerHTML = `
+<svg width="32" height="32" viewBox="0 0 24 24">
+  <rect x="6" y="5" width="4" height="14" fill="#1466E0"/>
+  <rect x="14" y="5" width="4" height="14" fill="#1466E0"/>
+</svg>`
+  } else {
+    buttonToggle.ariaLabel = "Start"
+    buttonToggle.innerHTML = `
+<svg width="30" height="30" viewBox="0 0 24 24">
+  <path d="M8 5v14l11-7z" fill="#1466E0"/>
+</svg>`
+  }
+}
+
 
 // Timer functions
 
@@ -41,13 +106,10 @@ function StopCountdown() {
   updateTime();
   updateInterface();
 
-  totalSeconds = initialTime;
-  timerDisplay.innerHTML = formatTime(totalSeconds);
+  enableEditMode();
 
-  return ("Countdown restarted");
+  return ("Countdown stopped");
 }
-
-// Logic functions
 
 function updateTime() {
   if (counting === true) {
@@ -59,30 +121,14 @@ function updateTime() {
   }
 }
 
-function updateInterface() {
-  // Aria Label
-  if (counting === true) {
-    buttonToggle.ariaLabel = "Pause"
-    buttonToggle.innerHTML = `
-<svg width="32" height="32" viewBox="0 0 24 24">
-  <rect x="6" y="5" width="4" height="14" fill="#1466E0"/>
-  <rect x="14" y="5" width="4" height="14" fill="#1466E0"/>
-</svg>`
-  } else {
-    buttonToggle.ariaLabel = "Start"
-    buttonToggle.innerHTML = `
-<svg width="30" height="30" viewBox="0 0 24 24">
-  <path d="M8 5v14l11-7z" fill="#1466E0"/>
-</svg>`
-  }
-}
+// Logic functions
 
 function countdown() {
-  console.log(formatTime(totalSeconds));
+  console.log(formatTime(totalSeconds))
   timerDisplay.innerHTML = formatTime(totalSeconds);
   totalSeconds -= 1;
 
-  if (totalSeconds < 1) { clearInterval(timer) };
+  if (totalSeconds < 0) { clearInterval(timer) };
 }
 
 function formatTime(timeInSeconds) {
